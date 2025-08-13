2ecb16a71bb3853b26e68d2b19f15149688fb265
boon <qxxpdada@outlook.com>
2025-08-14 01:08:56 +0800

    ✨ feat(PlayerDeathMixin): 改进虚空死亡检测逻辑，复活时给予钻石奖励 🔧 fix(deathvoid.mixins.json): 移除无效的mixin配置

--- src/main/java/org/boon/deathvoid/mixin/PlayerDeathMixin.java
+++ src/main/java/org/boon/deathvoid/mixin/PlayerDeathMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,19 +17,30 @@ public abstract class PlayerDeathMixin {
 
     private static final Logger LOGGER = LogManager.getLogger("PlayerVoidDeathMixin");
 
+    // 标记玩家是否虚空死亡
+    private boolean voidDeath = false;
+
     @Inject(method = "onDeath(Lnet/minecraft/entity/damage/DamageSource;)V", at = @At("HEAD"))
-    private void onPlayerDeath(DamageSource source, CallbackInfo ci) {
+    private void onDeath(DamageSource source, CallbackInfo ci) {
+        if ("outOfWorld".equals(source.getName())) {
+            voidDeath = true; // 标记虚空死亡
+            LOGGER.info("玩家掉入虚空，等待复活给予物品");
+        }
+    }
+
+    @Inject(method = "copyFrom", at = @At("TAIL"))
+    private void onRespawn(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
 
-        LOGGER.info("玩家死亡触发，原因: " + source.getName());
-
-        // 检测虚空死亡
-        if ("outOfWorld".equals(source.getName())) {
+        if (voidDeath) {
             // 给物品
             ItemStack stack = new ItemStack(Items.DIAMOND, 1);
             player.getInventory().insertStack(stack);
-            player.sendMessage(Text.of("掉入虚空，获得了钻石！"), false);
+            player.sendMessage(Text.of("掉入虚空，复活后获得了钻石！"), false);
 
+            LOGGER.info("玩家 " + player.getName().getString() + " 虚空死亡复活，给予物品成功");
+
+            voidDeath = false; // 清除标记
         }
     }
 }
