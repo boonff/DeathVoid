339e692891729570c56f2bd6f3222cfc86bd037d
boon <qxxpdada@outlook.com>
2025-08-14 00:54:45 +0800

    ✨ feat(Deathvoid.kt): 添加PlayerDeathListener注册逻辑

--- src/main/kotlin/org/boon/deathvoid/Deathvoid.kt
+++ src/main/kotlin/org/boon/deathvoid/Deathvoid.kt
@@ -3,10 +3,11 @@ package org.boon.deathvoid
 import net.fabricmc.api.ModInitializer
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.core.config.Configurator
+import org.boon.deathvoid.event.PlayerDeathListener
 
 class Deathvoid : ModInitializer {
 
     override fun onInitialize() {
-
+        PlayerDeathListener.register()
     }
 }
