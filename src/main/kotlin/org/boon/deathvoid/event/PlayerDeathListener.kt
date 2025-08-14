348ce67f6b5c052fa510993aaf5dc00f4d893e97
boon <qxxpdada@outlook.com>
2025-08-14 19:02:19 +0800

    ✨ feat(PlayerDeathListener): 添加 Fabric 环境类型检查并优化击杀者名称处理

--- src/main/kotlin/org/boon/deathvoid/event/PlayerDeathListener.kt
+++ src/main/kotlin/org/boon/deathvoid/event/PlayerDeathListener.kt
@@ -1,6 +1,8 @@
 package org.boon.deathvoid.event
 
+import net.fabricmc.api.EnvType
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents
+import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.server.network.ServerPlayerEntity
 import org.apache.logging.log4j.LogManager
 
@@ -8,6 +10,7 @@ object PlayerDeathListener {
     private val logger = LogManager.getLogger("PlayerDeathListener")
 
     fun register() {
+
         ServerEntityCombatEvents.AFTER_KILLED_OTHER_ENTITY.register { world, entity, killedEntity ->
             if (killedEntity is ServerPlayerEntity) {
                 val killerName = entity.name?.string ?: "环境"
